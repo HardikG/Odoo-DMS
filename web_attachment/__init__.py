a24870bf315cb2c7510266405a70e4d61ac5c625
Hardikgiri Goswami <hardikgiri.goswami@gmail.com>
2016-10-04 17:42:06 +0200

    [ADD]Added module.

---
+++ web_attachment/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# © 2016 Hardikgiri Goswami < hardikgiri.goswami@gmail.com >
---
+++ web_attachment/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2016 Hardikgiri Goswami < hardikgiri.goswami@gmail.com >
+
+
+{
+    "name": "Web Attachment",
+    "description": """
+    This module will add new UI for Attachment.
+    """,
+    "version": "9.0.1.0.0",
+    "category": "web",
+    "author": "Hardikgiri Goswami",
+    "website": "http://hardikg.github.io",
+    "license": "AGPL-3",
+    "depends": [
+        "web", "base"
+    ],
+    "data": [
+        # "views/templates.xml",
+    ],
+    'qweb': ["static/src/xml/form_extend.xml"],
+    'installable': True,
+}
