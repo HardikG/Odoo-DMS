e755cc46cba0f2036fb70eca8c1da3fc9ac403ec
Hardikgiri Goswami <hardikgiri.goswami@gmail.com>
2016-11-06 12:15:26 +0530

    [ADD]Added button and view for attachment.

--- web_attachment/static/src/js/attachment.js
+++ web_attachment/static/src/js/attachment.js
@@ -12,27 +12,27 @@ odoo.define('web.attachment', function (require) {
             this._super();
             this.$el.append("<button type='button' class='btn btn-default fa fa-paperclip oe_web_attachment'></button>");
             this.$(".oe_web_attachment").click(function(){
-                dialog = new Dialog(self, {
-                    title: _t('Attachment(s) for ' + self.dataset.model + " id : " + self.model_id), subtitle: '',
-                    size: 'large',
-                    dialogClass: '',
-                    $content: false,
-                    buttons: [{text: _t("Close"), close: true}]
-                });
-                console.log(self);
-                dialog.open();
+                self.open_attachments();
             });
         },
         open_attachments: function(){
+            var self=this;
             dialog = new Dialog(this, {
                 title: _t('Attachment(s)'), subtitle: '',
                 size: 'large',
                 dialogClass: '',
-                $content: false,
+                $content: QWeb.render("AttachmentView", {}),
                 buttons: [{text: _t("Close"), close: true}]
             });
-            console.log(this);
+            console.log("ddd",$(dialog.$content).find(".open-file-uploader"));
+            $(dialog.$content).find(".open-file-uploader")[0].click(function(){
+                alert("yo yo");
+                self.open_file_uploader();
+            });
             dialog.open();
         },
+        open_file_uploader: function(){
+            alert("Nailed It!!!!!");
+        }
     });
 });
