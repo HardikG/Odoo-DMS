c036375398043fc90e16671e8d6857701af2759e
Hardikgiri Goswami <hardikgiri.goswami@gmail.com>
2016-11-07 15:08:05 +0530

    [ADD]Added drag and drop functionality to the popup view.

--- web_attachment/static/src/js/attachment.js
+++ web_attachment/static/src/js/attachment.js
@@ -18,21 +18,78 @@ odoo.define('web.attachment', function (require) {
         open_attachments: function(){
             var self=this;
             dialog = new Dialog(this, {
-                title: _t('Attachment(s)'), subtitle: '',
+                title: _t('Attachment(s) for ' + self.dataset.model + " id : " + self.model_id),
                 size: 'large',
-                dialogClass: '',
-                $content: QWeb.render("AttachmentView", {}),
+                $content: $(QWeb.render("AttachmentView", {})),
                 buttons: [{text: _t("Close"), close: true}]
             });
-            console.log("ddd",$(dialog.$content).find(".open-file-uploader"));
-            $(dialog.$content).find(".open-file-uploader")[0].click(function(){
-                alert("yo yo");
+            dialog.open();
+            console.log(dialog.$content);
+            elem = dialog.$content[2];
+            console.log("elemmm",elem);
+            self.makeDroppable(elem,self.upload_file);
+            dialog.$content.find(".open-file-uploader").click(function(e){
                 self.open_file_uploader();
             });
-            dialog.open();
         },
         open_file_uploader: function(){
             alert("Nailed It!!!!!");
-        }
+        },
+        upload_file: function(e){
+            console.log("eeeee",e);
+        },
+        makeDroppable: function(element, callback) {
+
+            var input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('multiple', true);
+            input.style.display = 'none';
+
+            input.addEventListener('change', triggerCallback);
+            element.appendChild(input);
+
+            element.addEventListener('dragover', function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+                element.classList.add('dragover');
+            });
+            element.addEventListener('mouseover', function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+                element.classList.add('dragover');
+            });
+            element.addEventListener('mouseout', function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+                element.classList.remove('dragover');
+            });
+            element.addEventListener('dragleave', function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+                element.classList.remove('dragover');
+            });
+
+            element.addEventListener('drop', function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+                element.classList.remove('dragover');
+                triggerCallback(e);
+            });
+
+            element.addEventListener('click', function() {
+                input.value = null;
+                input.click();
+            });
+
+            function triggerCallback(e) {
+                var files;
+                if(e.dataTransfer) {
+                    files = e.dataTransfer.files;
+                } else if(e.target) {
+                    files = e.target.files;
+                }
+                callback.call(null, files);
+            }
+        },
     });
 });
